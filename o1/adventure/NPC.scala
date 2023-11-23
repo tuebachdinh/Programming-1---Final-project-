c3084c27320d782ba7b1f9c81b990f6b1772a25c
tuebachdinh <145895137+tuebachdinh@users.noreply.github.com>
2023-11-23 21:55:05 +0200

    update

---
+++ o1/adventure/NPC.scala
@@ -0,0 +1,11 @@
+package o1.adventure
+import scala.collection.mutable.Map
+
+/** The class "NPC" represent non-player characters in the text adventure games. 
+  * Each NPC has a name, a discription, and relationship with main player 
+  * This only appears in the third round of the game */
+
+class NPC(val name: String, val description: String):
+
+  override def toString = this.name
+
